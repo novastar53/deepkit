Fixed l2 diff computation

src/deepkit/internal_covariate_shift.py
@@ -87,9 +87,9 @@ def calc_ics(model: nnx.Module, optimizer: nnx.Optimizer, batch: jax.Array, labe
     conv_layer_id = int(wrt_layer.split(".")[1])
     wrt_layer_grad = grads["convs"][conv_layer_id] 
     shifted_grad = lookahead_grads["convs"][conv_layer_id]
-    l2_norm = jax.tree_util.tree_map(lambda x, y: jnp.linalg.norm(x - y), wrt_layer_grad, shifted_grad)
+    l2_diff = jax.tree_util.tree_map(lambda x, y: jnp.linalg.norm(x) -jnp.linalg.norm(y), wrt_layer_grad, shifted_grad)
     cos_angle = jax.tree_util.tree_map(cosine, wrt_layer_grad, shifted_grad)
     #print("l2_norm [conv 3]", l2_norm.conv.kernel.value)
     #print("cosine angle [conv 3]", cos_angle.conv.kernel.value)
 
-    return l2_norm, cos_angle
+    return l2_diff, cos_angle